bff584eecf22b9efd41bf04aa0813343a019aa56
enzoesper <enzoesper@hotmail.com>
2019-07-18 13:16:24 -0300

    applying basic routing between pages

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App'
 import "./css/style.css";
+import Router from './components/Router';
 
-ReactDOM.render(<App/>, document.querySelector('#main'));
+ReactDOM.render(<Router/>, document.querySelector('#main'));
 
