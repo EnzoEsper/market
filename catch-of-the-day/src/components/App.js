4fb0b7f280a6c581287a9b710902142890b85d39
enzoesper <enzoesper@hotmail.com>
2019-07-20 21:24:16 -0300

    initializing the app state component and updating it whenever a new fish is created (two components below)

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,13 +4,27 @@ import Inventory from './Inventory';
 import Order from './Order';
 
 class App extends React.Component {
+  state = {
+    fishes:{},
+    order:{}
+  };
+
+  addFish = fish => {
+    // Taking a copy of the existing state
+    const fishes = {...this.state.fishes};
+    // Adding our new fish to the copy of the state
+    fishes[`fish${Date.now()}`] = fish;
+    // Updating our state
+    this.setState({ fishes });
+  };
+
   render(){
     return(
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market"/>
         </div>
-        <Inventory />
+        <Inventory addFish={this.addFish}/>
         <Order />
       </div>
     )
